8428ad344e70e876060b8e89b5b553cb58926796
anhtudo97 <anhtudo97@gmail.com>
2021-07-08 22:59:09 +0700

    feat: add lib

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,19 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ComponentsModule } from './components/components.module';
 @Module({
-  imports: [ConfigModule.forRoot(), DatabaseModule],
+  imports: [
+    ConfigModule.forRoot(),
+    DatabaseModule,
+    GraphQLModule.forRoot({
+      playground: (process.env.NODE_ENV === 'development' && true) || false,
+      debug: (process.env.NODE_ENV === 'development' && true) || false,
+      autoSchemaFile: true,
+    }),
+    ComponentsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
